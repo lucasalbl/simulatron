449fa5f8173cd78c3ecc6ecfd8a3d6956cf6eb69
Lucas Alblas <lucas.alblas@gmail.com>
2020-11-03 21:34:56 +0100

    fixed wrong name

---
+++ cmake-build-debug/CMakeFiles/simulatron.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulatron.dir/human.cpp.obj"
+  "CMakeFiles/simulatron.dir/main.cpp.obj"
+  "simulatron.exe"
+  "simulatron.exe.manifest"
+  "simulatron.lib"
+  "simulatron.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simulatron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
